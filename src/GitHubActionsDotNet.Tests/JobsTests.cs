feae8f288ccd2279b1d8f157842bbb4b5a4de5c7
Sam Smith <samsmithnz@gmail.com>
2021-12-19 14:36:11 -0500

    Enabled basic tests

--- src/GitHubActionsDotNet.Tests/JobsTests.cs
+++ src/GitHubActionsDotNet.Tests/JobsTests.cs
@@ -46,365 +46,252 @@ jobs:
 
     }
 
-    //    [TestMethod]
-    //    public void SimpleVariablesJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //jobs:
-    //- job: Build
-    //  displayName: 'Build job'
-    //  pool:
-    //    vmImage: windows-latest
-    //  variables:
-    //    Variable1: 'new variable'
-    //  steps:
-    //  - task: CmdLine@2
-    //    inputs:
-    //      script: echo your commands here $(Variable1)
-    //";
+    [TestMethod]
+    public void SimpleVariablesJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  Build:
-    //    name: Build job
-    //    runs-on: windows-latest
-    //    env:
-    //      Variable1: new variable
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - run: echo your commands here ${{ env.Variable1 }}
-    //      shell: cmd
-    //";
+        //Assert
+        string expected = @"
+jobs:
+  Build:
+    name: Build job
+    runs-on: windows-latest
+    env:
+      Variable1: new variable
+    steps:
+    - uses: actions/checkout@v2
+    - run: echo your commands here ${{ env.Variable1 }}
+      shell: cmd
+    ";
 
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
 
-    //    }
+    }
 
 
 
-    //    [TestMethod]
-    //    public void ComplexVariablesWithComplexDependsOnJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //  jobs:
-    //  - job: Build
-    //    displayName: 'Build job'
-    //    pool:
-    //        vmImage: 'windows-latest'
-    //    dependsOn: 
-    //    - AnotherJob
-    //    variables:
-    //    - group: Active Login   # Contains codesigningCertPassword: Password for code signing cert
-    //    - name: sourceArtifactName
-    //      value: 'nuget-windows'
-    //    - name: targetArtifactName
-    //      value: 'nuget-windows-signed'
-    //    - name: pathToNugetPackages
-    //      value: '**/*.nupkg'
-
-    //    steps:
-    //    - task: CmdLine@2
-    //      inputs:
-    //        script: echo your commands here 
-    //";
-
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
-
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  Build:
-    //    name: Build job
-    //    runs-on: windows-latest
-    //    needs:
-    //    - AnotherJob
-    //    env:
-    //      group: Active Login
-    //      sourceArtifactName: nuget-windows
-    //      targetArtifactName: nuget-windows-signed
-    //      pathToNugetPackages: '**/*.nupkg'
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - run: echo your commands here
-    //      shell: cmd
-    //";
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
-
-    //    }
+    [TestMethod]
+    public void ComplexVariablesWithComplexDependsOnJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
 
-    //    [TestMethod]
-    //    public void ComplexVariablesWithSimpleDependsOnJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //  jobs:
-    //  - job: Build
-    //    displayName: 'Build job'
-    //    pool:
-    //        vmImage: 'windows-latest'
-    //    dependsOn: AnotherJob
-    //    variables:
-    //    - group: Active Login   # Contains codesigningCertPassword: Password for code signing cert
-    //    - name: sourceArtifactName
-    //      value: 'nuget-windows'
-    //    - name: targetArtifactName
-    //      value: 'nuget-windows-signed'
-    //    - name: pathToNugetPackages
-    //      value: '**/*.nupkg'
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
 
-    //    steps:
-    //    - task: CmdLine@2
-    //      inputs:
-    //        script: echo your commands here 
-    //";
+        //Assert
+        string expected = @"
+jobs:
+  Build:
+    name: Build job
+    runs-on: windows-latest
+    needs:
+    - AnotherJob
+    env:
+      group: Active Login
+      sourceArtifactName: nuget-windows
+      targetArtifactName: nuget-windows-signed
+      pathToNugetPackages: '**/*.nupkg'
+    steps:
+    - uses: actions/checkout@v2
+    - run: echo your commands here
+      shell: cmd
+    ";
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
+    }
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  Build:
-    //    name: Build job
-    //    runs-on: windows-latest
-    //    needs:
-    //    - AnotherJob
-    //    env:
-    //      group: Active Login
-    //      sourceArtifactName: nuget-windows
-    //      targetArtifactName: nuget-windows-signed
-    //      pathToNugetPackages: '**/*.nupkg'
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - run: echo your commands here
-    //      shell: cmd
-    //";
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
 
-    //    }
+    [TestMethod]
+    public void ComplexVariablesWithSimpleDependsOnJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
+
+
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
+
+        //Assert
+        string expected = @"
+jobs:
+  Build:
+    name: Build job
+    runs-on: windows-latest
+    needs:
+    - AnotherJob
+    env:
+      group: Active Login
+      sourceArtifactName: nuget-windows
+      targetArtifactName: nuget-windows-signed
+      pathToNugetPackages: '**/*.nupkg'
+    steps:
+    - uses: actions/checkout@v2
+    - run: echo your commands here
+      shell: cmd
+    ";
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
+
+    }
 
 
 
-    //    [TestMethod]
-    //    public void SimpleVariablesWithSimpleDependsOnJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //jobs:
-    //- job: Build
-    //  displayName: 'Build job'
-    //  pool:
-    //    vmImage: windows-latest
-    //  dependsOn: AnotherJob
-    //  variables:
-    //    Variable1: 'new variable'
-    //  steps:
-    //  - task: CmdLine@2
-    //    inputs:
-    //      script: echo your commands here $(Variable1)
-    //";
+    [TestMethod]
+    public void SimpleVariablesWithSimpleDependsOnJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  Build:
-    //    name: Build job
-    //    runs-on: windows-latest
-    //    needs:
-    //    - AnotherJob
-    //    env:
-    //      Variable1: new variable
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - run: echo your commands here ${{ env.Variable1 }}
-    //      shell: cmd
-    //";
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
 
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+        //Assert
+        string expected = @"
+jobs:
+  Build:
+    name: Build job
+    runs-on: windows-latest
+    needs:
+    - AnotherJob
+    env:
+      Variable1: new variable
+    steps:
+    - uses: actions/checkout@v2
+    - run: echo your commands here ${{ env.Variable1 }}
+      shell: cmd
+";
 
-    //    }
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
+
+    }
 
 
 
-    //    [TestMethod]
-    //    public void SimpleVariablesWithComplexDependsOnJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //jobs:
-    //- job: Build
-    //  displayName: 'Build job'
-    //  pool:
-    //    vmImage: windows-latest
-    //  dependsOn: 
-    //  - AnotherJob
-    //  variables:
-    //    Variable1: 'new variable'
-    //  steps:
-    //  - task: CmdLine@2
-    //    inputs:
-    //      script: echo your commands here $(Variable1)
-    //";
+    [TestMethod]
+    public void SimpleVariablesWithComplexDependsOnJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  Build:
-    //    name: Build job
-    //    runs-on: windows-latest
-    //    needs:
-    //    - AnotherJob
-    //    env:
-    //      Variable1: new variable
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - run: echo your commands here ${{ env.Variable1 }}
-    //      shell: cmd
-    //";
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
 
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+        //Assert
+        string expected = @"
+jobs:
+  Build:
+    name: Build job
+    runs-on: windows-latest
+    needs:
+    - AnotherJob
+    env:
+      Variable1: new variable
+    steps:
+    - uses: actions/checkout@v2
+    - run: echo your commands here ${{ env.Variable1 }}
+      shell: cmd
+";
 
-    //    }
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
 
-    //    [TestMethod]
-    //    public void CheckoutJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //jobs:
-    //- job: provisionProd
-    //  pool:
-    //    vmImage: ubuntu-latest
-    //  steps:
-    //  - checkout: self
-    //  - checkout: git://MyProject/MyRepo
-    //  - checkout: MyGitHubRepo # Repo declared in a repository resource
-    //";
+    }
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
+    [TestMethod]
+    public void CheckoutJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  provisionProd:
-    //    runs-on: ubuntu-latest
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - uses: actions/checkout@v2
-    //      with:
-    //        repository: git://MyProject/MyRepo
-    //    - uses: actions/checkout@v2
-    //      with:
-    //        repository: MyGitHubRepo
-    //";
 
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
 
-    //    }
+        //Assert
+        string expected = @"
+jobs:
+  provisionProd:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: actions/checkout@v2
+      with:
+        repository: git://MyProject/MyRepo
+    - uses: actions/checkout@v2
+      with:
+        repository: MyGitHubRepo
+";
 
-    //    [TestMethod]
-    //    public void CheckoutSimpleJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //jobs:
-    //- job: Build
-    //  pool:
-    //    vmImage: ubuntu-latest
-    //  steps:
-    //  - checkout: self
-    //    submodules: true
-    //";
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
+    }
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  Build:
-    //    runs-on: ubuntu-latest
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //";
+    [TestMethod]
+    public void CheckoutSimpleJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
 
-    //    }
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
 
-    //    [TestMethod]
-    //    public void EnvironmentJobTest()
-    //    {
-    //        //Arrange
-    //        Conversion conversion = new Conversion();
-    //        string yaml = @"
-    //jobs:
-    //- job: provisionProd
-    //  displayName: 'Provision Prod'
-    //  pool:
-    //    vmImage: ubuntu-latest
-    //  dependsOn: 
-    //  - functionalTestsStaging
-    //  environment: 
-    //    name: abelNodeDemoAppEnv.prod
-    //  steps:
-    //  - task: CmdLine@2
-    //    inputs:
-    //      script: echo hello world
-    //";
+        //Assert
+        string expected = @"
+jobs:
+  Build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+";
 
-    //        //Act
-    //        ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
 
-    //        //Assert
-    //        string expected = @"
-    //jobs:
-    //  provisionProd:
-    //    name: Provision Prod
-    //    runs-on: ubuntu-latest
-    //    needs:
-    //    - functionalTestsStaging
-    //    environment:
-    //      name: abelNodeDemoAppEnv.prod
-    //    steps:
-    //    - uses: actions/checkout@v2
-    //    - run: echo hello world
-    //      shell: cmd
-    //";
-    //        //url: https://abel-node-gh-accelerator.azurewebsites.net
+    }
 
-    //        expected = UtilityTests.TrimNewLines(expected);
-    //        Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+    [TestMethod]
+    public void EnvironmentJobTest()
+    {
+        //Arrange
+        GitHubActionsRoot root = new();
 
-    //    }
+
+        //Act
+        string yaml = Serialization.GitHubActionsSerialization.Serialize(root);
+
+        //Assert
+        string expected = @"
+jobs:
+  provisionProd:
+    name: Provision Prod
+    runs-on: ubuntu-latest
+    needs:
+    - functionalTestsStaging
+    environment:
+      name: prod
+    steps:
+    - uses: actions/checkout@v2
+    - run: echo hello world
+";
+        //url: https://abel-node-gh-accelerator.azurewebsites.net
+
+        expected = UtilityTests.TrimNewLines(expected);
+        Assert.AreEqual(expected, yaml);
+
+    }
 
 }
