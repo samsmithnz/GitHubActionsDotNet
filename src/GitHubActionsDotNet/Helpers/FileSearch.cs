f816726ec2154913425ef31517a037f7b1b79eb7
Sam Smith <samsmithnz@gmail.com>
2021-12-29 16:39:20 -0500

    Moved over Dependabot code

---
+++ src/GitHubActionsDotNet/Helpers/FileSearch.cs
@@ -0,0 +1,28 @@
+﻿using GitHubActionsDotNet.Common;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GitHubActionsDotNet.Helpers
+{
+    public class FileSearch
+    {
+        public static List<string> GetFilesForDirectory(string startingDirectory)
+        {
+            if (startingDirectory == null)
+            {
+                return new List<string>();
+            }
+            List<string> fileTypes = DependabotCommon.GetFileTypesToSearch();
+            List<string> sortedFiles = new List<string>();
+            foreach (string fileType in fileTypes)
+            {
+                string[] files = Directory.GetFiles(startingDirectory, fileType, SearchOption.AllDirectories);
+                //Order files alphabetically - there is some variation on different OS's on order
+                sortedFiles.AddRange(files.ToList<string>());
+            }
+            sortedFiles.Sort();
+            return sortedFiles;
+        }
+    }
+}
