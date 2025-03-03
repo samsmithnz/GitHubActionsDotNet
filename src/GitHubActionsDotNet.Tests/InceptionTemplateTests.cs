25bba9fad088635c4661b9c43561a7f7f30b7703
Sam Smith <samsmithnz@gmail.com>
2025-03-03 16:25:44 -0500

    updated .net steps

--- src/GitHubActionsDotNet.Tests/InceptionTemplateTests.cs
+++ src/GitHubActionsDotNet.Tests/InceptionTemplateTests.cs
@@ -27,7 +27,7 @@ echo ""CommitsSinceVersionSource: ${{ steps.gitversion.outputs.CommitsSinceVersi
             GitVersionStepHelper.AddGitVersionSetupStep(),
             GitVersionStepHelper.AddGitVersionDetermineVersionStep(),
             CommonStepHelper.AddScriptStep("Display GitVersion outputs", displayBuildGitVersionScript),
-            DotNetStepHelper.AddDotNetSetupStep("Setup .NET","7.x"),
+            DotNetStepHelper.AddDotNetSetupStep("Setup .NET","8.x"),
             DotNetStepHelper.AddDotNetTestStep(".NET test","src/GitHubActionsDotNet.Tests/GitHubActionsDotNet.Tests.csproj","Release",null,true),
             DotNetStepHelper.AddDotNetPackStep(".NET pack","src/GitHubActionsDotNet/GitHubActionsDotNet.csproj","Release",null,"--include-symbols -p:Version='${{ steps.gitversion.outputs.SemVer }}'", true),
             CommonStepHelper.AddUploadArtifactStep("Upload nuget package back to GitHub","nugetPackage","src/GitHubActionsDotNet/bin/Release","runner.OS == 'Linux'")
@@ -109,12 +109,12 @@ jobs:
       with:
         fetch-depth: 0
     - name: Setup GitVersion
-      uses: gittools/actions/gitversion/setup@v0.10.2
+      uses: gittools/actions/gitversion/setup@v3.1.11
       with:
         versionSpec: 5.x
     - name: Determine Version
       id: gitversion
-      uses: gittools/actions/gitversion/execute@v0.10.2
+      uses: gittools/actions/gitversion/execute@v3.1.11
     - name: Display GitVersion outputs
       run: |
         echo ""Version: ${{ steps.gitversion.outputs.SemVer }}""
@@ -122,7 +122,7 @@ jobs:
     - name: Setup .NET
       uses: actions/setup-dotnet@v3
       with:
-        dotnet-version: 7.x
+        dotnet-version: 8.x
     - name: .NET test
       run: dotnet test src/GitHubActionsDotNet.Tests/GitHubActionsDotNet.Tests.csproj -c Release
     - name: .NET pack
